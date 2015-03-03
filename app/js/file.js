10ef1fc2f629ca5d3d2619250bc0ea440f1510c1
Richard Armuelles <alexanderarmuelles@hotmail.com>
2015-03-03 10:22:01 -0500

    first push...

---
+++ app/js/file.js
@@ -0,0 +1,95 @@
+document.getElementById('toLoad').onclick = function(){
+  document.getElementById('toLoad-alert').style.display = "block";
+}
+
+document.getElementById('close-toLoad').onclick = function(){
+  document.getElementById('toLoad-alert').style.display = "none";
+}
+
+document.getElementById('saveFile').onclick = function(){
+  return saveFileAsText();
+}
+
+document.getElementById('saveHTML').onclick = function(){
+  return saveFileAsHTML();
+}
+
+function saveFileAsText() {
+
+  var textToWrite = document.getElementById('editor').value;
+  var textFileAsBlob = new Blob([textToWrite], {type:'text/x-markdown'});
+  var fileNameToSaveAs = document.getElementById('filename').value + '.md';
+
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+
+  if (window.webkitURL != null) {
+
+    // Chrome allows the link to be clicked
+    // without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  }
+
+  else {
+
+    // Firefox requires the link to be added to the DOM
+    // before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
+
+}
+
+function saveFileAsHTML() {
+  var header = '<html lang="en"><head><meta charset="UTF-8"><title>'+ document.getElementById('filename').value +'</title></head><body>';
+  var footer = '</body></html>'
+  var textToWrite = header + document.getElementById('output').innerHTML + footer;
+  var textFileAsBlob = new Blob([textToWrite], {type:'text/html'});
+  var fileNameToSaveAs = document.getElementById('filename').value + '.html';
+
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+
+  if (window.webkitURL != null) {
+
+    // Chrome allows the link to be clicked
+    // without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  }
+
+  else {
+
+    // Firefox requires the link to be added to the DOM
+    // before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
+
+}
+
+function destroyClickedElement(event) {
+  document.body.removeChild(event.target);
+}
+
+function loadFileAsText() {
+  var fileToLoad = document.getElementById("filetoload").files[0];
+
+  var fileReader = new FileReader();
+
+  fileReader.onload = function(fileLoadedEvent){
+    var textFromFileLoaded = fileLoadedEvent.target.result;
+    document.getElementById("editor").value = textFromFileLoaded;
+    document.getElementById('toLoad-alert').style.display = "none";
+  };
+  fileReader.readAsText(fileToLoad, "UTF-8");
+}
---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var open = require('gulp-open');
+
+gulp.task('test', shell.task(['node_modules/.bin/nw .']));
+
+gulp.task('browser', function(){
+  gulp.src('./app/*.html')
+  .pipe(open('file://<%= file.path %>', {app: 'firefox-aurora'}));g
+});
