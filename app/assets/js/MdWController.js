9094b2d08fb4f885e5feafbf7086b44fa5f32409
Richard Armuelles <alexanderarmuelles@hotmail.com>
2016-02-02 22:23:54 -0500

    better UI for email sending

--- app/assets/js/MdWController.js
+++ app/assets/js/MdWController.js
@@ -1,4 +1,4 @@
 var app = angular.module('MdWApp', [])
 .controller('MdWController', function($scope) {
-  
+
 });
--- app/assets/js/mail.js
+++ app/assets/js/mail.js
@@ -35,9 +35,52 @@ document.getElementById("send-email").onclick = function(){
   // send mail with defined transport object
   transporter.sendMail(options, function(error, info){
     if(error){
+
+      var errormsg = '<small class="error">Wrong entry<small>';
+
+      $('#to').addClass("error");
+      $(errormsg).insertAfter('#to');
+
+      $('#from').addClass("error");
+      $(errormsg).insertAfter('#from');
+
+      $('#password').addClass("error");
+      $(errormsg).insertAfter('#password');
+
       return console.log(error);
+
     }
-    console.log('Message sent: ' + info.response);
+
+    else {
+      console.log('Message sent: ' + info.response);
+
+      var modal = document.getElementById("email-alert");
+      modal.foundation('reveal','close');
+    }
+
   });
 
+}
+
+function removeError(){
+  if ( $( "small.error" ) ) {
+    $( "small.error" ).remove();
+
+    $('#to').removeClass("error");
+    $('#from').removeClass("error");
+    $('#password').removeClass("error");
+
+  }
+}
+
+document.getElementById("from").onclick = function(){
+  return removeError();
+}
+
+document.getElementById("to").onclick = function(){
+  return removeError();
+}
+
+document.getElementById("password").onclick = function(){
+  return removeError();
 }
